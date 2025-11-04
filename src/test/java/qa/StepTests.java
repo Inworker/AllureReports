719cee36a6c250c862620d55b6b227b9617d76b4
Inworker <31877506+Inworker@users.noreply.github.com>
2025-11-05 01:37:03 +0300

    fix

--- src/test/java/qa/StepTests.java
+++ src/test/java/qa/StepTests.java
@@ -7,14 +7,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.attachment;
-import static io.qameta.allure.Allure.step;
+import static io.qameta.allure.Allure.*;
 import static org.openqa.selenium.By.linkText;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -24,43 +24,57 @@ public class StepTests {
 
     @BeforeAll
     public static void setup() {
-        // Укажите путь к НОВОМУ ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "C:/ChromeTesting/chromedriver-win64/chromedriver.exe");
-
-        Configuration.browser = "chrome";
-        Configuration.browserBinary = "C:/ChromeTesting/chrome.exe";
-        Configuration.timeout = 10000;
-        // Отключить DevTools
-        Configuration.headless = false;
-        ChromeOptions options = new ChromeOptions();
-        // Добавляем аргументы для стабильности и полного экрана
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--start-maximized"); // Полный экран
-
+//        // Укажите путь к НОВОМУ ChromeDriver
+//        System.setProperty("webdriver.chrome.driver", "C:/ChromeTesting/chromedriver-win64/chromedriver.exe");
+//
+//        Configuration.browser = "chrome";
+//        Configuration.browserBinary = "C:/ChromeTesting/chrome.exe";
+//        Configuration.timeout = 10000;
+//        // Отключить DevTools
+//        Configuration.headless = false;
+//        ChromeOptions options = new ChromeOptions();
+//        // Добавляем аргументы для стабильности и полного экрана
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--remote-allow-origins=*");
+//        options.addArguments("--start-maximized"); // Полный экран
+//        step("открываем нужный репозиторий GitHub",
+//                () -> open("https://github.com/" + REPOSITORY));
         // Если хотим максимализировать окно, то используем "--start-maximized", но полный экран лучше через "--start-fullscreen"
+
     }
 
-    private static final String REPOSITORY = "eroshenkoam/allure-example";
-    private static final int ISSUE = 80;
-    private static final String BUG_TYPE = "bug";
+    private static final String REPOSITORY = "Inworker/AllureReports";
+    private static final int ISSUE = 1;
+    private static final String BUG_TYPE = "Open";
 
-
-    @Test
-    public void testLambdaStep() {
+    @BeforeEach
+    public void openPage() {
         System.out.println("ChromeDriver path: " + System.getenv("webdriver.chrome.driver"));
 
         SelenideLogger.addListener("allure", new AllureSelenide());
-        step("Открываем главную страницу", () -> {
-            open("https://github.com");
-        });
+        // Открытие страницы перед каждым тестом
+        step("открываем нужный репозиторий GitHub",
+                () -> open("https://github.com/" + REPOSITORY));
+    }
+    @Test
+    public void testLambdaStep() {
+
         step("кликаем по кнопке Issues ", () ->
                 $("#issues-tab [data-content='Issues']").click());
         step("проверяем название Issue", () ->
-                $("a[href='/taron19/Allure/issues/1']").shouldHave(Condition.exactText("FirstIssue")));
+                $("a[href='/Inworker/AllureReports/issues/1']").shouldHave(Condition.exactText("1")));
         step("проверяем что это тип ошибки" + BUG_TYPE,
                 () -> $$(".prc-Text-Text-0ima0").findBy(Condition.text(BUG_TYPE)).shouldBe(Condition.visible));
+    }
+
+    @Test
+    public void testLambdaAnnotatedStep() {
+        WebSteps steps = new WebSteps();
+        steps.takeScreenshot();
+        steps.openIssuesTab();
+        steps.shouldSeeIsueWithNumber("1");
+        steps.checkTabIssue("1");
     }
 
 }
---
+++ src/test/java/qa/WebSteps.java
@@ -0,0 +1,61 @@
+package qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+
+public class WebSteps {
+
+    @Step
+    public void openMainPage()
+    {
+        step("открываем нужный репозиторий GitHub",
+                () -> open("https://github.com/"));
+        attachment("Source", webdriver().driver().source());
+    }
+
+    @Step
+    public void searcForRepository(String repo)
+    {
+        // Открытие страницы перед каждым тестом
+        step("открываем нужный репозиторий GitHub",
+                () -> open("https://github.com/" + repo));
+    }
+
+
+    @Step
+    public void openIssuesTab()
+    {
+        step("кликаем по кнопке Issues ", () ->
+                $("#issues-tab [data-content='Issues']").click());
+    }
+
+    @Step
+    public void shouldSeeIsueWithNumber(String issue)
+    {
+        step("проверяем название Issue", () ->
+                $("a[href='/Inworker/AllureReports/issues/1']").shouldHave(Condition.exactText(issue)));
+    }
+
+    @Step
+    public void checkTabIssue(String issue)
+    {
+        step("проверяем что это тип ошибки" + issue,
+                () -> $$(".prc-Text-Text-0ima0").findBy(Condition.text(issue)).shouldBe(Condition.visible));
+
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot()
+    {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
